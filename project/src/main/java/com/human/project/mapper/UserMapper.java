d1dee23c8842d300a6c992c8c806082619637f52
SeungKyu37 <xmcmrb@naver.com>
2023-03-27 12:11:34 +0900

    2023-03-27

---
+++ project/src/main/java/com/human/project/mapper/UserMapper.java
@@ -0,0 +1,34 @@
+package com.human.project.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.human.project.domain.UserAuth;
+import com.human.project.domain.Users;
+
+@Mapper
+public interface UserMapper {
+	
+	// 회원가입
+	public int join(Users user) throws Exception;
+	
+	// 권한등록
+	public int insertAuth(UserAuth userAuth) throws Exception;
+	
+	// 회원조회
+	public Users select(Users user) throws Exception;
+	
+
+	// 아이디 찾기
+
+	
+
+	// 회원 목록
+	public List<Users> list() throws Exception;
+
+	// 회원 수정
+	public int update(Users user) throws Exception;
+	
+	// 회원 삭제
+}
